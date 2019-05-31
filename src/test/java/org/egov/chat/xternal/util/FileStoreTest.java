7e84ca81e11837dc5d694c47651cb2b65d722b85
Rushang Dhanesha <rushangdhanesha@gmail.com>
2019-05-31 14:36:48 +0530

    Filestore : convert file to Base64 : partial test
    Signed-off-by: Rushang Dhanesha <rushangdhanesha@gmail.com>

--- src/test/java/org/egov/chat/xternal/util/FileStoreTest.java
+++ src/test/java/org/egov/chat/xternal/util/FileStoreTest.java
@@ -1,10 +1,16 @@
 package org.egov.chat.xternal.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 import static org.junit.Assert.*;
 
@@ -15,18 +21,58 @@ public class FileStoreTest {
     @Test
     public void test() throws IOException {
 
-        FileStore fileStore = new FileStore();
-
+//        FileStore fileStore = new FileStore();
+//
 //        fileStore.downloadAndStore("http://www.pdf995.com/samples/pdf.pdf",
 //            "pb.amritsar", "rainmaker-pgr");
-
-        File tempFile = fileStore.getFileAt("http://www.pdf995.com/samples/pdf.pdf");
-
+//
+//        File tempFile = fileStore.getFileAt("http://www.pdf995.com/samples/pdf.pdf");
+//
 //        File file = new File(FileStoreTest.class.getClassLoader() + "/tmp/qwe/asd.pdf");
-
-        log.info(tempFile.getAbsolutePath());
-
+//
+//        log.info(tempFile.getAbsolutePath());
+//
 //        tempFile.delete();
+    }
+
+
+    @Test
+    public void fileBase64TransformTest() throws IOException {
+
+        String filename = "/home/rushang/Downloads/Sandbox user guide.pdf";
+        File file = new File(filename);
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        File f2 = new File(filename + ".enc");
+        FileOutputStream fileOutputStream = new FileOutputStream(f2);
+        OutputStream outputStream = Base64.getEncoder().wrap(fileOutputStream);
+
+        int _byte;
+        while ((_byte = fileInputStream.read()) != -1)
+        {
+            outputStream.write(_byte);
+        }
+
+        outputStream.close();
+
+//        FileInputStream fileInputStream1 = new FileInputStream(f2);
+//
+//        String string = new String( FileUtils.readFileToByteArray(f2) );
+//
+//        System.out.println(string);
+//
+//        f2.delete();
+
+    }
+
+    @Test
+    public void readEncodedFile() throws IOException {
+        String filename = "/home/rushang/Downloads/Sandbox user guide.pdf.enc";
+
+        System.out.println(FileUtils.sizeOf(new File(filename)));
+
+
     }
 
 }
